8436f8ff6938b576f47e5bc708248a6ba2ba8ea5
sanctum32 <andriuspel@gmail.com>
2015-04-06 08:22:16 +0300

    Silence errors about missing cannot train text id for trade skills and pet trainers
    Signed-off-by: sanctum32 <andriuspel@gmail.com>

--- src/arcemu-world/ObjectMgr.cpp
+++ src/arcemu-world/ObjectMgr.cpp
@@ -1804,7 +1804,7 @@ void ObjectMgr::LoadTrainers()
             tr->Can_Train_Gossip_TextId = 1;
         }
 
-        if (!NpcTextStorage.LookupEntry(tr->Cannot_Train_GossipTextId))
+        if (!NpcTextStorage.LookupEntry(tr->Cannot_Train_GossipTextId) && (tr->TrainerType != TRAINER_TYPE_TRADESKILLS || tr->TrainerType != TRAINER_TYPE_PET))
         {
             Log.Error("ObjectMgr", "Tried to load trainer_defs for npc %u but \"can not train gossip textid\" is incorrect, overriting to 1");
             tr->Cannot_Train_GossipTextId = 1;
